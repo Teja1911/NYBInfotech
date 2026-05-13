5d9e56af46ba182c3a1f272d6f7b9e6fb7cf990c
tejaswar rao <tejaswar.rao@nybinfotech.com>
2026-05-13 17:40:18 +0530

    inital commit

---
+++ JavaScript/13_05_2026(Modules)/index.js
@@ -0,0 +1,24 @@
+import good from './index1.js'
+console.log(good)
+export default function sum(a,b){
+    return a+b
+}
+console.log(sum(1,2))
+
+export let arr=[1,2,3]
+
+export function hello(){
+
+}
+
+export function diff(){
+
+}
+
+function bye(){
+
+}
+
+// 2 Ways
+// Default  only 1 export can be done 
+// Named export n number 
---
+++ JavaScript/13_05_2026(Modules)/index1.js
@@ -0,0 +1,9 @@
+import s from './index.js'
+console.log(s)
+
+export default function good(){
+
+}
+
+import {hello,diff} from './index.js'
+console.log(hello)
